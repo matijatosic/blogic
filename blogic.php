d1ceb7e4760926c1daf03392d6a5f9b69b76bfba
Matija Tosic <tosic.matija@gmail.com>
2017-06-28 10:32:57 +0200

    separating general fetching function

--- blogic.php
+++ blogic.php
@@ -1,15 +1,21 @@
 <?php
 
+function fetchFromTableById($table, $id)
+{
+    $rows = fetchQueryResults('SELECT * FROM ' . $table . ' WHERE id = ' . $id . ';');
+    return $rows;
+}
+
 
 function fetchPostById($id)
 {
-    $posts = fetchQueryResults('SELECT * FROM posts WHERE id = ' . $id . ';');
+    $posts = fetchFromTableById('posts', $id);
     return $posts[0];
 }
 
 function fetchUserById($id)
 {
-    $users = fetchQueryResults('SELECT * FROM users WHERE id = ' . $id . ';');
+    $users = fetchFromTableById('users', $id);
     return $users[0];
 }
 
